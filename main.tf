d923ba8deed7cba99c8b7d05829a50ca92e22a32
nana kwadwo <nampong32@gmail.com>
2024-07-04 11:24:16 -0400

    iam user added

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.57.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+resource "aws_iam_user" "devops" {
+  name = "nana.a"
+
+
+}
+
+
+
